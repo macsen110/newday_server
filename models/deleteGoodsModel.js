d26bc3316d44409e43b99b9b3fb8166d32264fbd
liangyusen <liangyusen1202@gmail.com>
2017-12-20 16:55:17 +0800

    优化zone

--- models/deleteGoodsModel.js
+++ models/deleteGoodsModel.js
@@ -1,5 +1,10 @@
 var myModel = require('./goodsModelConnection');
+var CmyModel = require('./commentsModelConnection');
 module.exports = function (goodsid) {
+	CmyModel
+		.remove({C_goodid: goodsid})
+		.then()
+		.catch()
 	return myModel
 		.remove({_id: goodsid})
 		.then(item => {return {code: 0}})

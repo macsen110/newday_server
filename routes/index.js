6aad7f05061df73b29dddc6239e4ab924fbcda6d
macsen110 <839945193@qq.com>
2019-07-14 20:13:04 +0800

    UPDATE LOGIN STATUS

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,23 @@
-module.exports  = function(app) {
-	var express = require('express');
-	var router = express.Router();
-	function resolveAfter2Seconds(x) {
-  	return new Promise(resolve => {
-			setTimeout(() => {
-				resolve(x);
-			}, 0);
-		});
-	}
-	router.use('/home', async (req, res, next) => {
-		var a = await resolveAfter2Seconds(req.session.user)
-		// res.json({
-		// 	code: 0,
-		// 	isLogin: a ? true : false,
-		// 	user: a
-		// })
-		res.json({
-			url: 'https://www.macsen318.com/zip/electron-quick-start-10.0.0-mac.zip'
-		});
-	})
-	return router;
+module.exports = function(app) {
+  var express = require("express");
+  var router = express.Router();
+  function resolveAfter2Seconds(x) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(x);
+      }, 0);
+    });
+  }
+  router.use("/home", async (req, res, next) => {
+    var a = await resolveAfter2Seconds(req.session.user);
+    res.json({
+      code: 0,
+      isLogin: a ? true : false,
+      user: a
+    });
+    // res.json({
+    // 	url: 'https://www.macsen318.com/zip/electron-quick-start-10.0.0-mac.zip'
+    // });
+  });
+  return router;
 };
